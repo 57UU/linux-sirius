546da42a6ef08147bbacba8fac748617fcf09da0
Richard Acayan <mailingradian@gmail.com>
2026-04-06 08:13:50 -0400

    Reapply "pmdomain: core: Leave powered-on genpds on until sync_state"
    This reverts commit 3b9f1b57b4fc570b85d38e635a7033557de815fb.
    
    Amends: Revert "pmdomain: core: Leave powered-on genpds on until sync_state"
    Signed-off-by: Richard Acayan <mailingradian@gmail.com>

--- drivers/pmdomain/core.c
+++ drivers/pmdomain/core.c
@@ -3510,6 +3510,7 @@ void of_genpd_sync_state(struct device_node *np)
 	list_for_each_entry(genpd, &gpd_list, gpd_list_node) {
 		if (genpd->provider == of_fwnode_handle(np)) {
 			genpd_lock(genpd);
+			genpd->stay_on = false;
 			genpd_power_off(genpd, false, 0);
 			genpd_unlock(genpd);
 		}
@@ -3537,6 +3538,7 @@ static void genpd_provider_sync_state(struct device *dev)
 
 	case GENPD_SYNC_STATE_SIMPLE:
 		genpd_lock(genpd);
+		genpd->stay_on = false;
 		genpd_power_off(genpd, false, 0);
 		genpd_unlock(genpd);
 		break;
